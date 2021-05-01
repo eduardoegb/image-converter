85f20a4c4397340cc90528c000cd1a2612b0fdfb
Eduardo Gonzalez <eduardoegb@gmail.com>
2021-05-01 13:41:20 -0400

    Converter: Adding extra validation

--- image_converter.py
+++ image_converter.py
@@ -3,19 +3,27 @@ from PIL import Image
 
 
 def converter(argv):
-    input_folder = argv[1]
-    output_folder = argv[2]
+    if len(argv) < 4:
+        print('Option \'converter\' requires input and output directories, please read documentation')
+    else:
+        input_folder = argv[1]
+        output_folder = argv[2]
 
-    if not os.path.exists(output_folder):
-        os.mkdir(output_folder)
-        print(f'Directory \'{output_folder}\' was created')
+        if not os.path.exists(input_folder):
+            print(f'Directory \'{input_folder}\' does not exists, please enter a valid folder path')
+        else:
+            if not os.path.exists(output_folder):
+                os.mkdir(output_folder)
+                print(f'Directory \'{output_folder}\' was created')
 
-    for file in os.listdir(input_folder):
-        file_name = os.path.splitext(file)[0]
-        file_ext = os.path.splitext(file)[1]
-        file_exists = os.path.exists(f'{output_folder}/{file}')
-        if file_ext == '.jpg' and not file_exists:
-            img = Image.open(f'{input_folder}/{file}')
-            img.save(f'{output_folder}/{file_name}.png', 'png')
+            print('Converting...')
 
-    print('All done!')
+            for file in os.listdir(input_folder):
+                file_name = os.path.splitext(file)[0]
+                file_ext = os.path.splitext(file)[1]
+                file_exists = os.path.exists(f'{output_folder}/{file}')
+                if file_ext == '.jpg' and not file_exists:
+                    img = Image.open(f'{input_folder}/{file}')
+                    img.save(f'{output_folder}/{file_name}.png', 'png')
+
+            print('All done!')
--- main.py
+++ main.py
@@ -2,4 +2,8 @@ import sys
 
 from image_converter import converter
 
-converter(sys.argv)
+option = sys.argv[1]
+if option == 'converter':
+	converter(sys.argv)
+else:
+	print(f'Option \'{option}\' is invalid, please read documentation')
